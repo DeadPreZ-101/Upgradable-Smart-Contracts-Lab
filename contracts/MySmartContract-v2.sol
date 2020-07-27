0b018da8b932ca37c53436b25de1448bb6810618
DeadPrez-101 <al.chemist.jasat@gmail.com>
2020-07-27 13:19:29 -0400

    Test Case Issues Fixed

--- contracts/MySmartContract-v2.sol
+++ contracts/MySmartContract-v2.sol
@@ -1,17 +1,53 @@
+// // SPDX-License-Identifier: MIT
+
+// pragma solidity ^0.6.0;
+
+
+// contract MySmartContract {
+//     uint32 public counter;
+//     address private owner;
+
+//     /**
+//     @dev Enforces the caller to be the contract's owner.
+//     */
+//     modifier isOwner {
+//         require(msg.sender == owner, "Sender is not owner.");
+//         _;
+//     }
+
+//     constructor(uint32 _counter) public {
+//         counter = _counter; // Allows setting counter's initial value on deployment.
+//         // Sets the contract's owner as the address that deployed the contract.
+//         owner = msg.sender;
+//     }
+
+//     /**
+//     @notice Increments the contract's counter if contract is active.
+//     @dev It will revert is the contract is stopped. Create a modifier "isNotStopped"
+//     */
+//     function incrementCounter() public {
+//         counter++; // Fixes bug introduced in version 1.
+//     }
+// }
+
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
-
 contract MySmartContract {
     uint32 public counter;
     address private owner;
-
+    bool private pause = false;
     /**
     @dev Enforces the caller to be the contract's owner.
     */
     modifier isOwner {
         require(msg.sender == owner, "Sender is not owner.");
+        _;
+    }
+    modifier pauseContract() {
+        require(!pause, "Contract is stopped.");
         _;
     }
 
@@ -25,7 +61,11 @@ contract MySmartContract {
     @notice Increments the contract's counter if contract is active.
     @dev It will revert is the contract is stopped. Create a modifier "isNotStopped"
     */
-    function incrementCounter() public {
+    function incrementCounter() public pauseContract {
         counter++; // Fixes bug introduced in version 1.
+    }
+
+    function toggleContractStopped() public isOwner {
+        pause = !pause;
     }
 }
--- test/MySmartContract-v1.test.js
+++ test/MySmartContract-v1.test.js
@@ -1,14 +1,12 @@
-const {expect} = require("chai");
-const {expectRevert} = require("@openzeppelin/test-helpers");
-
+const { expect } = require("chai");
+const { expectRevert } = require("@openzeppelin/test-helpers");
 
 const MySmartContract = artifacts.require("MySmartContractV1");
 
 // This tests version 1 of MySmartContract.
-contract("MySmartContract", function([_, deployer ]) {
-
-  it("increments counter", async function() {
-    this.contract = await MySmartContract.new({from: deployer});
+contract("MySmartContract", function ([_, deployer]) {
+  it("increments counter", async function () {
+    this.contract = await MySmartContract.new({ from: deployer });
 
     await this.contract.incrementCounter();
     let counter = await this.contract.counter.call();
@@ -19,10 +17,12 @@ contract("MySmartContract", function([_, deployer ]) {
     expect(counter.toNumber()).to.equal(4);
   });
 
-  it("reverts when stopped", async function() {
-    this.contract = await MySmartContract.new({from: deployer});
-    await this.contract.toggleContractStopped({from: deployer});
-    await expectRevert(this.contract.incrementCounter(), "Contract is stopped.");
+  it("reverts when stopped", async function () {
+    this.contract = await MySmartContract.new({ from: deployer });
+    await this.contract.toggleContractStopped({ from: deployer });
+    await expectRevert(
+      this.contract.incrementCounter(),
+      "Contract is stopped."
+    );
   });
-
 });
